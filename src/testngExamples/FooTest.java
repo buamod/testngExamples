0bbd516e0089212b64760861a99f3eaf1a293afa
Ibrahim Buamod <ibrahim_buamod@trendmicro.com>
2017-07-24 14:43:03 -0400

    added some comments

--- src/testngExamples/FooTest.java
+++ src/testngExamples/FooTest.java
@@ -1,5 +1,7 @@
 package testngExamples;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Factory;
@@ -18,29 +20,42 @@ public class FooTest {
     @DataProvider(name = "dp")
     public Object[][] createData() {
         return new Object[][] {
-            { "Cedric" },
-            { "Martina" }
+            { "Stable" },
+            { "Edge" }
         };
     }
 
     @Factory(dataProvider = "dp")
     public Object[]  testFactory(String name) {
-        return new Object[] { new FooTest(name) };
+        return new FooTest[] { new FooTest(name) };
     }
  
-    @BeforeTest
-    public void setOptions(XmlTest test)
-    {
-     test.setGroupByInstances(true);
-    }
+        @BeforeTest
+        public void setOptions(XmlTest test)
+        {
+            test.setGroupByInstances(true);
+        }
+        
+        @BeforeClass
+        public void setEnvironment()
+        {
+        	System.out.println(name);
+        }
 
-    @Test
-    public void test1() throws InterruptedException {
-      System.out.println("test1 " + name);
-    }
+        @AfterClass
+        public void removeEnvironment()
+        {
+        	System.out.println(name+" will be removed");
+        }
 
-    @Test
-    public void test2() throws InterruptedException {
-     System.out.println("test2 " + name);
-     }
+
+        @Test
+        public void test1() throws InterruptedException {
+            System.out.println("test1 " + name);
+        }
+
+        @Test
+        public void test2() throws InterruptedException {
+            System.out.println("test2 " + name);
+        }
    }
