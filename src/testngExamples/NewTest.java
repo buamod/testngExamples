fc7d61d38f61af22fd2c6b2deeb44a4955e24e29
Ibrahim <buamod@gmail.com>
2018-02-11 17:51:47 -0500

    old commit I didn't push

--- src/testngExamples/NewTest.java
+++ src/testngExamples/NewTest.java
@@ -2,21 +2,37 @@ package testngExamples;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 
 public class NewTest {
-  @Test(dataProvider = "dp")
-  public void f(Integer n, String s) {
+  private String type;
+
+@Test
+  public void firstTest() {
+	  System.out.println("the first test case with the type : "+ type);
   }
 
-  @DataProvider
-  public Object[][] dp() {
-    return new Object[][] {
-      new Object[] { 1, "a" },
-      new Object[] { 2, "b" },
-    };
+@Test
+  public void secondTest() {
+	  System.out.println("the second test case with the type : "+ type);
   }
+  
+  @Factory(dataProvider = "data-provider", dataProviderClass = dataProviderClass.class) //(dataProvider="dataProviderMethod")
+  public NewTest(String type){
+      this.type=type;
+      System.out.println("test the = "+type);
+  }
+
+//  @DataProvider(parallel=false)
+//  public static Object[][] dataProviderMethod() {
+//	  
+//	  return new Object[][] {
+//		    new Object[] { "Stable" },
+//		    new Object[] { "Edge" }};
+//  }
+  
   @BeforeClass
   public void beforeClass() {
   }
---
+++ src/testngExamples/dataProviderClass.java
@@ -0,0 +1,15 @@
+package testngExamples;
+
+import org.testng.annotations.DataProvider;
+
+public class dataProviderClass {
+	
+	  @DataProvider(name = "data-provider")
+	    public static Object[][] dataProviderMethod() 
+	    {
+	        return new Object[][] { { "data one" }, { "data two" } };
+	    }
+
+}
+
+
